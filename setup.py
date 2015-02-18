eb677cd5df372e970dae4ae8ce88f9346aa77870
Daniel Waeber <daniel.waeber@gameduell.de>
2015-02-18 16:55:38 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     version = "0.1",
     packages = find_packages(),
     scripts = [],
-
+    install_requires=['dill>=0.2',
+                      'pandas>=0.14'],
     # metadata for upload to PyPI
     author = "Philipp Metzner",
     author_email = "philipp.metzner@gameduell.de",
     description = "parallel version of some pandas function",
     license = "MIT",
     keywords = "pandas mutliprocessing parallel",
-    url = "https://github.com/GameDuell/parallelpandas", requires=['dill', 'pandas']
+    url = "https://github.com/GameDuell/parallelpandas",
 )
